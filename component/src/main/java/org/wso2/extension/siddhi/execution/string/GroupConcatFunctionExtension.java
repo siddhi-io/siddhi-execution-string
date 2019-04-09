82a6dd63f34963011b1958ae7180a0fbca627486
sujanan <sujananbhathiya@gmail.com>
2019-04-09 11:11:19 +0530

    Upgrade siddhi version

--- component/src/main/java/org/wso2/extension/siddhi/execution/string/GroupConcatFunctionExtension.java
+++ component/src/main/java/org/wso2/extension/siddhi/execution/string/GroupConcatFunctionExtension.java
@@ -1,52 +1,47 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
- * You may obtain a copy of the License at
+ * you may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.wso2.extension.siddhi.execution.string;
 
-import org.wso2.siddhi.annotation.Example;
-import org.wso2.siddhi.annotation.Extension;
-import org.wso2.siddhi.annotation.Parameter;
-import org.wso2.siddhi.annotation.ReturnAttribute;
-import org.wso2.siddhi.annotation.util.DataType;
-import org.wso2.siddhi.core.config.SiddhiAppContext;
-import org.wso2.siddhi.core.executor.ConstantExpressionExecutor;
-import org.wso2.siddhi.core.executor.ExpressionExecutor;
-import org.wso2.siddhi.core.query.selector.attribute.aggregator.AttributeAggregator;
-import org.wso2.siddhi.core.util.config.ConfigReader;
-import org.wso2.siddhi.query.api.definition.Attribute;
-import org.wso2.siddhi.query.api.exception.SiddhiAppValidationException;
+import io.siddhi.annotation.Example;
+import io.siddhi.annotation.Extension;
+import io.siddhi.annotation.Parameter;
+import io.siddhi.annotation.ReturnAttribute;
+import io.siddhi.annotation.util.DataType;
+import io.siddhi.core.config.SiddhiQueryContext;
+import io.siddhi.core.executor.ConstantExpressionExecutor;
+import io.siddhi.core.executor.ExpressionExecutor;
+import io.siddhi.core.query.processor.ProcessingMode;
+import io.siddhi.core.query.selector.attribute.aggregator.AttributeAggregatorExecutor;
+import io.siddhi.core.util.config.ConfigReader;
+import io.siddhi.core.util.snapshot.state.State;
+import io.siddhi.core.util.snapshot.state.StateFactory;
+import io.siddhi.query.api.definition.Attribute;
+import io.siddhi.query.api.exception.SiddhiAppValidationException;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 import java.util.TreeMap;
 
-/**
- * groupConcat(string1, 'separator', bool, 'ASC/DESC')
- * Returns concated string for all the events separated by the given separator.
- * Accept Type(s): STRING. The string that need to be aggregated.
- * Accept Type(s): STRING. The separator that separates each string that gets aggregated.
- * Accept Type(s): BOOL. To only have distinct string keys in the the aggregation..
- * Accept Type(s): STRING. Accepts 'ASC' or 'DESC' strings to sort the string keys by ascending or descending order.
- * Return Type(s): STRING
- */
+import static io.siddhi.query.api.definition.Attribute.Type.BOOL;
+import static io.siddhi.query.api.definition.Attribute.Type.STRING;
 
 @Extension(
         name = "groupConcat",
@@ -91,168 +86,200 @@ import java.util.TreeMap;
         }
 
 )
-public class GroupConcatFunctionExtension extends AttributeAggregator {
+public class GroupConcatFunctionExtension
+        extends AttributeAggregatorExecutor<GroupConcatFunctionExtension.AggregatorState> {
 
-    private Attribute.Type returnType = Attribute.Type.STRING;
-    private Map<Object, Integer> dataSet;
+    private Map<Object, Integer> dataMap = new LinkedHashMap<>();
+
     private boolean distinct = false;
-    private boolean canDistroy = true;
 
     @Override
-    protected void init(ExpressionExecutor[] attributeExpressionExecutors, ConfigReader configReader,
-                        SiddhiAppContext siddhiAppContext) {
-        if (attributeExpressionExecutors.length < 1) {
-            throw new SiddhiAppValidationException("str:groupConcat() function requires at mandatory `key` attribute,"
-                    + " but found no attributes.");
-        } else if (attributeExpressionExecutors.length > 4) {
-            throw new SiddhiAppValidationException("str:groupConcat() function requires only `key`, `separator`, " +
-                    "`distinct`, and `order` as attributes,"
-                    + " but found " + attributeExpressionExecutors.length + " attributes");
+    protected StateFactory<AggregatorState> init(ExpressionExecutor[] expressionExecutors,
+                                                 ProcessingMode processingMode,
+                                                 boolean b,
+                                                 ConfigReader configReader,
+                                                 SiddhiQueryContext siddhiQueryContext) {
+        int executorsCount = expressionExecutors.length;
+
+        if (executorsCount < 1) {
+            throw new SiddhiAppValidationException("str:groupConcat() function requires at "
+                    + "mandatory `key` attribute, but found no attributes.");
+        }
+        if (executorsCount > 4) {
+            throw new SiddhiAppValidationException("str:groupConcat() function requires only "
+                    + "`key`, `separator`, `distinct`, and `order` as attributes, but found "
+                    + attributeExpressionExecutors.length + " attributes");
         }
 
-        if (attributeExpressionExecutors.length >= 2) {
-            if (attributeExpressionExecutors[1].getReturnType() != Attribute.Type.STRING) {
-                throw new SiddhiAppValidationException("str:groupConcat() function's second attribute `separator` " +
-                        "should be a `STRING`, but found " + attributeExpressionExecutors[1].getReturnType() + " .");
+        switch (executorsCount) {
+            case 4: {
+                ExpressionExecutor executor = expressionExecutors[3];
+                if (isNotType(executor, STRING)) {
+                    throw new SiddhiAppValidationException("str:groupConcat() function's forth attribute `order` "
+                            + "should be a constant `STRING` having `'ASC'` or `'DESC'` values, "
+                            + "but found " + executor.getReturnType() + " .");
+                }
+                if (isNotConstantAttribute(executor)) {
+                    throw new SiddhiAppValidationException("str:groupConcat() function's forth attribute `order` "
+                            + "should be a constant having `'ASC'` or `'DESC'`, but found a variable attribute.");
+                }
+                String value = ((String) ((ConstantExpressionExecutor) executor).getValue()).toUpperCase();
+                switch (value) {
+                    case "ASC":
+                        dataMap = new TreeMap<>();
+                        break;
+                    case "DESC":
+                        dataMap = new TreeMap<>(Collections.reverseOrder());
+                        break;
+                    default:
+                        throw new SiddhiAppValidationException("str:groupConcat() function's forth attribute `order` "
+                                + "should be a constant `'ASC'` or `'DESC'`, but found '" + value + "'.");
+                }
+            }
+            /* Fall through */
+            case 3: {
+                ExpressionExecutor executor = expressionExecutors[2];
+                if (isNotType(executor, BOOL)) {
+                    throw new SiddhiAppValidationException("str:groupConcat() function's third attribute `distinct` "
+                            + "should be a constant `BOOL`, but found " + executor.getReturnType() + " .");
+                }
+                if (isNotConstantAttribute(executor)) {
+                    throw new SiddhiAppValidationException("str:groupConcat() function's third attribute `distinct` "
+                            + "should be a constant, but found a variable attribute.");
+                }
+                distinct = (Boolean) ((ConstantExpressionExecutor) executor).getValue();
+            }
+            /* Fall through */
+            case 2: {
+                ExpressionExecutor executor = expressionExecutors[1];
+                if (isNotType(executor, STRING)) {
+                    throw new SiddhiAppValidationException("str:groupConcat() function's second attribute `separator` "
+                            + "should be a `STRING`, but found " + executor.getReturnType() + " .");
+                }
             }
         }
-        if (attributeExpressionExecutors.length >= 3) {
-            if (attributeExpressionExecutors[2].getReturnType() != Attribute.Type.BOOL) {
-                throw new SiddhiAppValidationException("str:groupConcat() function's third attribute `distinct` " +
-                        "should be a constant `BOOL`, but found " + attributeExpressionExecutors[2].getReturnType()
-                        + " .");
-            }
-            if (!(attributeExpressionExecutors[2] instanceof ConstantExpressionExecutor)) {
-                throw new SiddhiAppValidationException("str:groupConcat() function's third attribute `distinct` " +
-                        "should be a constant, but found a variable attribute.");
-            }
-            distinct = (Boolean) ((ConstantExpressionExecutor) attributeExpressionExecutors[2]).getValue();
-        }
-        if (attributeExpressionExecutors.length >= 4) {
-            if (attributeExpressionExecutors[3].getReturnType() != Attribute.Type.STRING) {
-                throw new SiddhiAppValidationException("str:groupConcat() function's forth attribute `order` " +
-                        "should be a constant `STRING` having `'ASC'` or `'DESC'` values, " +
-                        "but found " + attributeExpressionExecutors[3].getReturnType() + " .");
-            }
-            if (!(attributeExpressionExecutors[3] instanceof ConstantExpressionExecutor)) {
-                throw new SiddhiAppValidationException("str:groupConcat() function's forth attribute `order` " +
-                        "should be a constant having `'ASC'` or `'DESC'`, but found a variable attribute.");
-            }
-            String value = (String) ((ConstantExpressionExecutor) attributeExpressionExecutors[3]).getValue();
-            if ("ASC".equalsIgnoreCase(value)) {
-                dataSet = new TreeMap<Object, Integer>();
-            } else if ("DESC".equalsIgnoreCase(value)) {
-                dataSet = new TreeMap<Object, Integer>(Collections.reverseOrder());
-            } else {
-                throw new SiddhiAppValidationException("str:groupConcat() function's forth attribute `order` " +
-                        "should be a constant `'ASC'` or `'DESC'`, but found '" + value + "'.");
-            }
-        } else {
-            dataSet = new LinkedHashMap<>();
-        }
+
+        return () -> new AggregatorState();
+    }
+
+    private boolean isNotType(ExpressionExecutor executor, Attribute.Type type) {
+        return executor.getReturnType() != type;
+    }
+
+    private boolean isNotConstantAttribute(ExpressionExecutor executor) {
+        return !(executor instanceof ConstantExpressionExecutor);
     }
 
     @Override
-    public Attribute.Type getReturnType() {
-        return returnType;
-    }
-
-    @Override
-    public Object processAdd(Object o) {
+    public Object processAdd(Object o, AggregatorState aggregatorState) {
         addString(String.valueOf(o));
+        aggregatorState.makeUnDestroyable();
         return constructConcatString(",");
     }
 
     @Override
-    public Object processAdd(Object[] objects) {
+    public Object processAdd(Object[] objects, AggregatorState aggregatorState) {
         addString(String.valueOf(objects[0]));
+        aggregatorState.makeUnDestroyable();
         return constructConcatString(String.valueOf(objects[1]));
     }
 
     @Override
-    public Object processRemove(Object o) {
+    public Object processRemove(Object o, AggregatorState aggregatorState) {
         removeString(String.valueOf(o));
+        if (dataMap.size() == 0) {
+            aggregatorState.makeDestroyable();
+        }
         return constructConcatString(",");
     }
 
     @Override
-    public Object processRemove(Object[] objects) {
+    public Object processRemove(Object[] objects, AggregatorState aggregatorState) {
         removeString(String.valueOf(objects[0]));
+        if (dataMap.size() == 0) {
+            aggregatorState.makeDestroyable();
+        }
         return constructConcatString(String.valueOf(objects[1]));
     }
 
     private void addString(String data) {
-        Integer count = dataSet.get(data);
-        if (count == null) {
-            dataSet.put(data, 1);
-        } else {
-            dataSet.put(data, ++count);
-        }
-        canDistroy = false;
+        Integer count = dataMap.get(data);
+        dataMap.put(data, (count == null) ? 1 : count + 1);
     }
 
     private void removeString(String data) {
-        Integer count = dataSet.get(data);
+        Integer count = dataMap.get(data);
         if (count == 1) {
-            dataSet.remove(data);
+            dataMap.remove(data);
         } else if (count > 1) {
-            dataSet.put(data, --count);
-        }
-        if (dataSet.size() == 0) {
-            canDistroy = true;
+            dataMap.put(data, count - 1);
         }
     }
 
     private Object constructConcatString(String separator) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringJoiner joiner = new StringJoiner(separator);
+
         if (distinct) {
-            for (Iterator<Object> iterator = dataSet.keySet().iterator(); iterator.hasNext(); ) {
-                Object key = iterator.next();
-                stringBuilder.append(key);
-                if (iterator.hasNext()) {
-                    stringBuilder.append(separator);
-                }
+            for (Object key : dataMap.keySet()) {
+                joiner.add(String.valueOf(key));
             }
-        } else {
-            for (Iterator<Map.Entry<Object, Integer>> iterator = dataSet.entrySet().iterator();
-                 iterator.hasNext(); ) {
-                Map.Entry<Object, Integer> entry = iterator.next();
-                for (int i = 0; i < entry.getValue(); i++) {
-                    stringBuilder.append(entry.getKey());
-                    if (i != entry.getValue() - 1) {
-                        stringBuilder.append(separator);
-                    }
-                }
-                if (iterator.hasNext()) {
-                    stringBuilder.append(separator);
-                }
+            return joiner.toString();
+        }
+
+        for (Map.Entry<Object, Integer> entry : dataMap.entrySet()) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                joiner.add(String.valueOf(entry.getKey()));
             }
         }
-        return stringBuilder.toString();
-    }
 
-
-    @Override
-    public boolean canDestroy() {
-        return canDistroy;
+        return joiner.toString();
     }
 
     @Override
-    public Object reset() {
-        dataSet.clear();
-        canDistroy = true;
+    public Object reset(AggregatorState aggregatorState) {
+        dataMap.clear();
+        aggregatorState.makeDestroyable();
         return "";
     }
 
     @Override
-    public Map<String, Object> currentState() {
-        HashMap<String, Object> state = new HashMap<>();
-        state.put("dataSet", dataSet);
-        return state;
+    public Attribute.Type getReturnType() {
+        return STRING;
     }
 
-    @Override
-    public void restoreState(Map<String, Object> state) {
-        dataSet = (Map<Object, Integer>) state.get("dataSet");
+    class AggregatorState extends State {
+
+        private static final String KEY_DATA_MAP = "dataMap";
+
+        private final Map<String, Object> state = new HashMap<>();
+
+        private boolean canDestroy = true;
+
+        private AggregatorState() {
+            state.put(KEY_DATA_MAP, dataMap);
+        }
+
+        private void makeDestroyable() {
+            canDestroy = true;
+        }
+
+        private void makeUnDestroyable() {
+            canDestroy = false;
+        }
+
+        @Override
+        public boolean canDestroy() {
+            return canDestroy;
+        }
+
+        @Override
+        public Map<String, Object> snapshot() {
+            return state;
+        }
+
+        @Override
+        public void restore(Map<String, Object> map) {
+            dataMap = (Map<Object, Integer>) map.get(KEY_DATA_MAP);
+        }
     }
 }
